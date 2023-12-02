39ecaeb3b2eb93428d714e8d24f0f5bfdba80479
Amol Venkataraman <amolven16@gmail.com>
2023-12-02 00:25:37 -0500

    Comments for Day 2 Python

--- Python/day2p1.py
+++ Python/day2p1.py
@@ -1,16 +1,19 @@
+# Maximum available cubes
 MAX_CUBES = {"red": 12, "green": 13, "blue": 14}
 
-ans = 0
+ans = 0 # Answer
 
+# Open file and iterate through the lines
 with open("input2.txt", "r") as f:
     for line in f.readlines():
-        valid = True
-        draws = line.rstrip().split(": ")[1].split("; ")
+        valid = True # Assume game is valid at first
+        draws = line.rstrip().split(": ")[1].split("; ") # Get each individual set of draws
         for draw in draws:
+            # Get the number of cubes of each colour and set valoid to false if it exceeds the maximum
             cols = [i.split(" ") for i in draw.split(", ")]
             for c in cols:
                 if int(c[0]) > MAX_CUBES[c[1]]: valid = False
         
-        if valid: ans += int(line.split(": ")[0].split(" ")[1])
+        if valid: ans += int(line.split(": ")[0].split(" ")[1]) # Update the final answer if the game is valid
 
 print(ans)
--- Python/day2p2.py
+++ Python/day2p2.py
@@ -1,16 +1,19 @@
 import math
 
-ans = 0
+ans = 0 # Answer
 
+# Open file and iterate through the lines
 with open("input2.txt", "r") as f:
     for line in f.readlines():
+        # Minimum number of cubes of each type for the game
         min_cubes = {"red": 0, "green": 0, "blue": 0}
-        draws = line.rstrip().split(": ")[1].split("; ")
+        draws = line.rstrip().split(": ")[1].split("; ") # Get each individual set of draws
         for draw in draws:
+            # Get the number of cubes of each colour and update min_cubes if needed
             cols = [i.split(" ") for i in draw.split(", ")]
             for c in cols:
                 min_cubes[c[1]] = max(min_cubes[c[1]], int(c[0]))
         
-        ans += math.prod([min_cubes[i] for i in min_cubes])
+        ans += math.prod([min_cubes[i] for i in min_cubes]) # Update the final answer
 
 print(ans)
